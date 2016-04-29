c2cbfb6c84c6234334d5fd4204de80d1ac5ddfb8
Gianluca <gl051@biccari.com>
2016-04-28 18:12:01 -0700

    Improved readability

--- test-anagram.py
+++ test-anagram.py
@@ -16,18 +16,18 @@ class AnagramTestCase(unittest.TestCase):
     # -----------------------------------
     # METHOD                    Check if
     # -----------------------------------
-    # assertEqual(a, b)	        a == b
-    # assertNotEqual(a, b)	    a != b
+    # assertEqual(a, b)         a == b
+    # assertNotEqual(a, b)      a != b
     # assertTrue(x)	bool(x)     is True
-    # assertFalse(x)	        bool(x) is False
-    # assertIs(a, b)	        a is b
-    # assertIsNot(a, b)	        a is not b
-    # assertIsNone(x)	        x is None
-    # assertIsNotNone(x)	    x is not None
-    # assertIn(a, b)	        a in b
-    # assertNotIn(a, b)	        a not in b
-    # assertIsInstance(a, b)	isinstance(a, b)
-    # assertNotIsInstance(a, b)	not isinstance(a, b)
+    # assertFalse(x)            bool(x) is False
+    # assertIs(a, b)            a is b
+    # assertIsNot(a, b)         a is not b
+    # assertIsNone(x)           x is None
+    # assertIsNotNone(x)        x is not None
+    # assertIn(a, b)            a in b
+    # assertNotIn(a, b)         a not in b
+    # assertIsInstance(a, b)    isinstance(a, b)
+    # assertNotIsInstance(a, b) not isinstance(a, b)
 
     # Each test should test a single, specific property of the code
     # and be name accordingly
